5cf1967cddd148f454375b4b0be7b847e5b05bf5
Nicholas-Fisher <avidnick@gmail.com>
2015-02-05 13:38:59 -0500

    nicks version

--- sorting.py
+++ sorting.py
@@ -1,6 +1,18 @@
 import sys
 from z3 import *
 
+def z3sort(i,X):
+   if len(X) == 1:
+      return [Y[i] == X[0]]
+   else:
+      return [Y[i] == X[0]] + z3sort(i, X[1:])
+
+#def z3sort(n,i,X):
+#   if len(X) == 1:
+#      return [Y[i] == X[0]]
+#   else:
+#      return [(Y[i] == X[0], And([Y[z] != X[0] for z in range(n)])] + z3sort(i, X[1:])
+
 # Checks the usage in the command line
 if len(sys.argv) != 2:
     print("Usage: python sorting.py <input_file>")
@@ -37,18 +49,33 @@ Y = [Int("y%i" % i) for i in range(n)]
 # The final formula going in. Change this to your actual formula
 
 # Set X constraints
-X_const = [X[i] == in_list[i] for i in range(n)]
+X_const = ([X[i] == in_list[i] for i in range(n)])
 
 # Set Y constraints
-Y_const = [And(Y[i] < Y[i+1]) for i in range(n-1)]
-for i in range(n):
-	Y_const = Y_const + [Or(tuple(Y[i] == X[j] for j in range(n)))]
+Y_const1 = ([(Y[i] <= Y[i+1]) for i in range(n-1)])
 
-F = X_const + Y_const
+# Set Y constraints
+#Y2_const = [Or(tuple(Y[i] == X[j] for i in range(n-1) for j in range(n-1)))]
+#Y2_const = [Or(tuple(Y[i] == X[1:] for i in range(n-1)))]
+
+Y_const2 = [Or(z3sort(i,X)) for i in range(n)]
+
+
+
+#Y_const = Y2_const + Y1_const
+#Y_const = Y1_const + Y2_const
+
+
+#for i in range(n):
+#	Y_const = Y_const + [Or(tuple(Y[i] == X[j] for j in range(n)))]
+
+F = And(X_const + Y_const1 + Y_const2)
 
 # debugging purposes
 print 'X constraint:', X_const
-print 'Y constraint:', Y_const
+print 'Y1 constraint:', Y_const1
+print 'Y2 constraint:', Y_const2
+print F
 
 
 #########################################################
@@ -68,3 +95,8 @@ if isSAT == sat:
 else:
     print("Inconceivable! The specification must always be satisfiable.")
 
+# def z3sort(X):
+#    if X=[]:
+#       return 0
+#    else:
+#       return X[0] + z3sum(X[1:])
