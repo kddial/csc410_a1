b591ac4c09de1457fd04326b2d40882219ec94c8
Nicholas-Fisher <avidnick@gmail.com>
2015-02-05 20:27:03 -0500

    clean version

--- sorting.py
+++ sorting.py
@@ -2,27 +2,6 @@ import sys
 from z3 import *
 from itertools import permutations
 
-#def z3sort(n,i,X):
-#   l = []
-#   for z in range(n):
-#   		if z != i:
-#   			l.append(Y[z] != X[0])
-#
-#   if len(X) == 1:
-#      return [And([Y[i] == X[0]] + l)]
-#   else:
-#      return [And([Y[i] == X[0]] + l)] + z3sort(n, i, X[1:])
-#      #return [(Y[i] == X[0]), And(l)] + z3sort(n, i, X[1:])
-
-# def z3sort(P,X):
-#    #if len(X) == 1:
-#    i = P[0]
-#    if len(X) == 1:
-#       return [Y[i] == X[0]]
-#    else:
-#       return [Y[i] == X[0]] + z3sort(P[1:], X[1:])
-#       #return [(Y[i] == X[0]), And(l)] + z3sort(n, i, X[1:])
-
 # Checks the usage in the command line
 if len(sys.argv) != 2:
     print("Usage: python sorting.py <input_file>")
@@ -53,7 +32,6 @@ Y = [Int("y%i" % i) for i in range(n)]
 #        The actual constraints for the problem         #
 #########################################################
 
-
 ##################  Your Code Here  #####################
 
 items = range(n)
@@ -68,30 +46,8 @@ X_const = ([X[i] == in_list[i] for i in range(n)])
 
 # Set Y constraints
 Y_const1 = ([(Y[i] <= Y[i+1]) for i in range(n-1)])
-
-# Set Y constraints
-#Y2_const = [Or(tuple(Y[i] == X[j] for i in range(n-1) for j in range(n-1)))]
-#Y2_const = [Or(tuple(Y[i] == X[1:] for i in range(n-1)))]
-
-#Y_const2 = [And([Y[P[j][i]] == X[i] for i in range(n)]) for j in range(len(P) - 1)]
-#Y2_const = [Or(tuple(Y[i] == X[j] for i in range(n-1) for j in range(n-1)))]
-#Y_const2 =  [[And([Y[i] == X[P[j][i]] for j in range(len(P)-1)] for i in range(n))]]
-#Y_const2 = [And([z3sort(list(P[i]),X) for i in range(len(P))])]
-
 Y_const2 =  [And([Y[i] == X[P[j][i]]  for i in range(n) ]) for j in range(len(P)-1)]
 
-#Y_const2 = [Or(z3sort(0,X)) for i in range(n)]
-#Y_const2 = And([Or(z3sort(i,X, n-1)) for i in range(n)])
-#Y_const2 = [Or([And(z3sort(n,i,X)) for i in range(n)])]
-
-#Y_const = Y2_const + Y1_const
-#Y_const = Y1_const + Y2_const
-
-
-#for i in range(n):
-#	Y_const = Y_const + [Or(tuple(Y[i] == X[j] for j in range(n)))]
-
-#F = And(X_const + Y_const1 + Y_const2)
 F = And(X_const + Y_const1 + [Or(Y_const2)])
 # debugging purposes
 print 'X constraint:', X_const
